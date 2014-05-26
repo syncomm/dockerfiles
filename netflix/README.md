1c7a9500c430238a8b24d3c9f3497d6653010cda
Gregory S. Hayes <ghayes@redhat.com>
2014-05-26 04:24:03 -0400

    added netflix container

--- netflix/README.md
+++ netflix/README.md
@@ -2,4 +2,3 @@
 
 Currently a work-in-progress to enable a Netflix player through PipeLight and FireFox.
 Unfortunately, it is very bloated at the time, and sound work needs to be completed. 
-Also, work is needed to enable HW accelerated video.
--- netflix/start-netflix.sh
+++ netflix/start-netflix.sh
@@ -4,6 +4,6 @@ xauth add $XCOOKIE
 ln -s /tmp/dri/ /dev/dri
 ln -s /tmp/video0 /dev/video0
 ln -s /tmp/snd/ /dev/snd   
-# WINE=/usr/bin/wine /usr/share/pipelight/pipelight-hw-accel --disable
+WINE=/usr/bin/wine /usr/share/pipelight/pipelight-hw-accel --disable
 firefox http://www.netflix.com
 

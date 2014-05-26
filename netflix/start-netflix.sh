c0154aefaaa7773e74149f54eba6d982cde926c4
Gregory S. Hayes <ghayes@redhat.com>
2014-05-26 03:11:06 -0400

    added netflix container

---
+++ netflix/start-netflix.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+touch /.Xauthority
+xauth add $XCOOKIE
+ln -s /tmp/dri/ /dev/dri
+ln -s /tmp/video0 /dev/video0
+ln -s /tmp/snd/ /dev/snd   
+# WINE=/usr/bin/wine /usr/share/pipelight/pipelight-hw-accel --disable
+firefox http://www.netflix.com
+

dcc74b50fde94d5b5d2d9fb4bf244c9fef01f6f7
Gregory S. Hayes <ghayes@redhat.com>
2014-05-30 18:55:48 -0400

    Disable screensaver while running

--- netflix/docker-netflix.sh
+++ netflix/docker-netflix.sh
@@ -16,10 +16,18 @@ lpurp='\e[1;35m'
 yellow='\e[1;33m'
 NC='\e[0m' # No Color
 
-echo -e "${lpurp}Grabbing X11 Cookie of host${NC}" 
+# Sop screensaver (need to detect gsettings)
+echo -e "${lpurp}Stopping Screensaver${NC}" 
+xset s off
+xset s noblank
+xset -dpms
+gsettings set org.gnome.desktop.session idle-delay 0
+
 # Get the X11 Cookie to pass
+echo -e "${lpurp}Grabbing X11 Cookie of host${NC}" 
 XCOOKIE=`xauth list | grep unix | cut -f2 -d"/" | tr -cd '\11\12\15\40-\176' | sed -e 's/  / /g'`
 
+# Create the Pulseaudio Socket
 if [ ! -e /tmp/.pulse-socket ];
 then
     echo -e "${lpurp}Adding Pulseaudio socket at /tmp/.pulse-socket${NC}" 
@@ -28,4 +36,16 @@ fi
 
 echo -e "${lpurp}Launching syncomm/netflix container${NC}" 
 echo sudo docker run --rm -e XCOOKIE=\'$XCOOKIE\' -v /tmp/.X11-unix/:/tmp/.X11-unix/ -v /tmp/.pulse-socket:/tmp/.pulse-socket -t syncomm/netflix | sh
+
+# Resume screensaver
+echo -e "${lpurp}Resuming Screensaver${NC}" 
+xset s off
+xset s noblank
+xset -dpms
+gsettings set org.gnome.desktop.session idle-delay 300
+
+# Remove Pulseaudio socket
+# echo -e "${lpurp}Removing Pulseaudio socket${NC}" 
+# pactl unload-module module-native-protocol-unix
+
 exit 0

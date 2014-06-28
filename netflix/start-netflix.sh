0bfe17acd5dc7b3c90a9a3eae17d2129eb40e011
Gregory S. Hayes <ghayes@redhat.com>
2014-06-28 01:03:56 -0400

    Version update, clearly state license

--- netflix/start-netflix.sh
+++ netflix/start-netflix.sh
@@ -1,14 +1,30 @@
 #! /bin/bash
-##################################################################
-# Script: start-netflix.sh
-# Version: 0.2.0
-#
-# Description:
-# The script to start netflix inside the container
-# 
-# by Gregory S. Hayes <ghayes@redhat.com>
-#
-##################################################################
+
+#########################################################################
+# Script: start-netflix.sh                                              #
+# Version: 0.2.1                                                        #
+#                                                                       #
+# Description:                                                          #
+# The script to start netflix inside the container                      #
+#                                                                       #
+# Copyright (C) 2014, Gregory S. Hayes <ghayes@redhat.com>              #
+#                                                                       #
+# This program is free software; you can redistribute it and/or modify  #
+# it under the terms of the GNU General Public License as published by  #
+# the Free Software Foundation; either version 2 of the License, or     #
+# (at your option) any later version.                                   #
+#                                                                       #
+# This program is distributed in the hope that it will be useful,       #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+# GNU General Public License for more details.                          #
+#                                                                       #
+# You should have received a copy of the GNU General Public License     #
+# along with this program; if not, write to the                         #
+# Free Software Foundation, Inc.,                                       #
+# 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
+#                                                                       #
+#########################################################################
 
 # Set some colors
 red='\e[0;31m'

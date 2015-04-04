531b56c003a245583cd4117b82ed2d188567c5b1
syncomm <syncomm@dune.cloud-ninja.org>
2015-04-03 23:04:27 -0400

    Added SELinux policy

---
+++ spotify/selinux/install-docker-spotify-policy.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Must be run as root"
+    exit
+fi
+
+echo "Compiling docker-spotify SELinux module"
+checkmodule -M -m -o docker-spotify.mod docker-spotify.te
+echo "Packaging module"
+semodule_package -o docker-spotify.pp -m docker-spotify.mod
+echo "Installing module"
+# semodule -i docker-spotify.pp
